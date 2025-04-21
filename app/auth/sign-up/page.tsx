ac6e86c87f4c2ddbd45a2357efbc076cf52231d4
Richard Gibbons <richard@digitalapplied.com>
2025-04-21 13:18:51 +0200

    Update auth pages / components

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,13 +1,11 @@
-import { SignUpForm } from "@/components/sign-up-form";
+import { SignUpForm } from '@/components/sign-up-form'
 
-export const metadata = { title: "Sign up · Supabase Example" };
-
-export default async function Page() {
+export default function Page() {
   return (
-    <div className="flex flex-1 w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <SignUpForm />
       </div>
     </div>
-  );
+  )
 }
